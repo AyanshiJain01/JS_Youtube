f7d62653af15009851752c755f63d900927e5b97
Ayanshi Jain <ayanshijain7@gmail.com>
2025-05-08 10:52:23 +0000

    variables.js file

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "Ayanshi@gmail.com"
+var accountPassword = "Ayanshi@12#"
+accountCity = "Jaipur"
+let accountState;
+
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+/*accountId = 2345 //can't be changed because of the constant keyword */
+
+accountEmail = "Aya@gmailcom"
+accountPassword = '123456'
+accountCity = "Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
