178f549109e3f80b59bf9e4bf10a4e0141b28f09
Ayanshi Jain <ayanshijain7@gmail.com>
2025-05-15 13:01:00 +0000

    conversion and operation

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,34 @@
+// let score = "33abs"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score) //score yha pr number me convert ho gya h
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let someNumber = 33
+// let stringNum = String(someNumber);
+// console.log(stringNum);
+// console.log(typeof someNumber)
+// console.log(typeof stringNum)
+
+
+// ****************** Operations **************** //
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Ayanshi"
+let str2 = " Jain"
+let str3 = str1 + str2
+console.log(str3)
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
